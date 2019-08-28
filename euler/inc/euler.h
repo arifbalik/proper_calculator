9c0d0def893fec3bc67c2f750e08727500c95fd4
Arif Balik <arifbalik@outlook.com>
2019-08-29 02:02:39 +0300

    structs moved to euler.h

--- euler/inc/euler.h
+++ euler/inc/euler.h
@@ -1,16 +1,13 @@
+#ifndef _EULER_
+#define _EULER_
+
 #include <math.h>
 #include <stdint.h>
-#define MAX_QUERY_LENGTH 80
-#include "../parser/symbol_table.h"
-#include "../parser/ast.h"
-#include "_atof.h"
-#include "_itoa.h"
-#include "_strcpy.h"
-#include "_strlen.h"
-#include "_strstr.h"
-#include "strplace.h"
 
-//#define UNIX
+#define MAX_QUERY_LENGTH 80
+#define MAX_AST_BRANCH 100
+
+#define UNIX
 
 #ifdef UNIX
 #include <stdio.h>
@@ -66,15 +63,51 @@ typedef union {
 	char *var;
 } numerical_t;
 
+/* AST Structre */
+typedef struct ast {
+	uint8_t type; /* This is the token value in grammar.h */
+
+	/* Value of the token */
+	union {
+		double number;
+		char literal;
+	} value;
+
+	struct ast *left; /* left hand side of the node */
+	struct ast *right; /* right hand side of the node */
+} ast_t;
+
+/* Symbol Table Structre */
+typedef struct {
+	/* This holds the beggininng of matched token.
+	 * Look for next index to get the end
+	 */
+	char *p;
+
+	uint8_t no; /* Hold token number, defined in grammar.h */
+	uint8_t priority; /* For partial parsing */
+
+} __attribute__((packed)) token_t;
+
+typedef struct {
+	token_t token[MAX_QUERY_LENGTH];
+	uint8_t top; /* index of last element in the stack */
+	uint8_t cur; /* currently handled token index */
+
+} __attribute__((packed)) symbol_table_t;
+
 typedef struct {
 	etype_t type; /* Developer should check this type before any action */
 	numerical_t resultn; /* Numerical results will be stored here */
 	estatus_t status; /* Developer can use this to debug the engine */
 	char *ascii; /* The query */
 	double (*func)(void);
-	symbol_table_t symbol_table;
+	symbol_table_t symbol_table; /* Token table */
+	ast_t ast[MAX_AST_BRANCH]; /* Abstract Syntax Tree */
 
 } __attribute__((packed)) ersl_t;
 
 void parse_query(ersl_t *ersl);
 void euler_init(void);
+
+#endif /* _EULER_ */
