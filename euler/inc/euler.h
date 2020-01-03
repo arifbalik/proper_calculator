e6762411180d50ac8cabbdd3ef09ff9a8dc4741b
Arif Balik <arifbalik@outlook.com>
2020-01-03 06:51:02 +0300

    comment make up

--- euler/inc/euler.h
+++ euler/inc/euler.h
@@ -97,14 +97,14 @@ typedef struct {
 } __attribute__((packed)) symbol_table_t;
 
 typedef struct {
-	etype_t type; /* Developer should check this type before any action */
-	numerical_t resultn; /* Numerical results will be stored here */
-	estatus_t status; /* Developer can use this to debug the engine */
-	char *ascii; /* The query */
+	etype_t type; 			/* Developer should check this type before any action */
+	numerical_t resultn; 		/* Numerical results will be stored here */
+	estatus_t status; 		/* Developer can use this to debug the engine */
+	char *ascii; 			/* The query */
 	double (*func)(void);
-	symbol_table_t symbol_table; /* Token table */
-	ast_t *ast[MAX_AST_BRANCH]; /* Abstract Syntax Tree */
-	uint8_t ast_top_idx; /* index of top node */
+	symbol_table_t symbol_table; 	/* Token table */
+	ast_t *ast[MAX_AST_BRANCH]; 	/* Abstract Syntax Tree */
+	uint8_t ast_top_idx; 		/* index of top node */
 
 	/* Reserved Abstract Syntax Tree.
 	 * Only used by euler in custom malloc operations
